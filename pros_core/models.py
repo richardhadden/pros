7fa19cede4084bcafc4911480c854edb01b93b88
Richard Hadden <richard.hadden@oeaw.ac.at>
2022-06-17 16:19:35 +0200

    save-- not working, but leaving code in

--- pros_core/models.py
+++ pros_core/models.py
@@ -1,4 +1,12 @@
-from neomodel import StructuredNode, UniqueIdProperty, StringProperty
+from collections import defaultdict
+from email.policy import default
+
+from neomodel import StructuredNode, UniqueIdProperty, StringProperty, db
+
+
+def unpack_result(result):
+    relation, node = result
+    return relation.type.lower(), node._properties
 
 
 class ProsNode(StructuredNode):
@@ -9,3 +17,13 @@ class ProsNode(StructuredNode):
     def save(self):
         self.real_type = type(self).__name__.lower()
         super().save()
+
+    def get_all_related(self):
+        """DOES NOT REALLY DO WHAT IS WANTED"""
+        results, meta = self.cypher("MATCH (p)<-[r]->(q) WHERE id(p)=$self RETURN r,q")
+        unpacked_results = defaultdict(list)
+        for result in results:
+            relation, node = unpack_result(result)
+            unpacked_results[relation].append(node)
+
+        return unpacked_results
