2d01790a9cf13e1baa2d3da671f8aac2557f8b75
Richard Hadden <richard.hadden@oeaw.ac.at>
2023-01-11 13:10:08 +0100

    save

--- test2.py
+++ test2.py
@@ -1,25 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+from icecream import ic
 
 
 @dataclass
-class ResponseWrapper:
+class Thing:
     """Class for keeping track of an item in inventory."""
 
     data: dict
     status: int = 200
 
-    def keys(self):
-        return ["dict", "status"]
 
-    def __getitem__(self, key):
-        return self.__dict__.get(key)
-
-    def __iter__(self):
-        yield from (self.data, self.status)
-
-
-t = ResponseWrapper({"something": 1})
-
-data, status = t
-
-print(data, status)
+t = Thing({"one": "two"}, 300)
+ic(asdict(t))
