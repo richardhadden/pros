ae16f8bb526b2778a5b16b53f01911a8c546a56b
Richard Hadden <richard.hadden@oeaw.ac.at>
2023-03-13 16:22:50 +0100

    save

--- interface/src/components/ui_components/entityChip.tsx
+++ interface/src/components/ui_components/entityChip.tsx
@@ -13,105 +13,111 @@ const EntityChip: Component<{
   onClick?: (e: MouseEvent) => void;
   isDeleted?: boolean;
   deletedAndHasDependentNodes?: boolean;
+  glow?: boolean;
 }> = (props) => {
+  createEffect(() => console.log(props.glow));
+
   const style =
     "text-neutral-content p-3 max-w-4xl mb-3 mr-3 pr-4 rounded-sm h-12 prose-md relative top-1.5 font-semibold inline-block h-fit z-10";
   return (
-    <Switch>
-      <Match when={props.href && props.isDeleted}>
-        <UnsavedLink
-          class={
-            style +
-            `  w-fit cursor-pointer  bg-gray-400 pr-2 transition-all hover:bg-gray-500`
-          }
-          href={props.href}
-        >
-          <div class="flex flex-row">
+    <>
+      {props.glow ? "GLOW" : ""}
+      <Switch>
+        <Match when={props.href && props.isDeleted}>
+          <UnsavedLink
+            class={
+              style +
+              `  w-fit cursor-pointer  bg-gray-400 pr-2 transition-all hover:bg-gray-500`
+            }
+            href={props.href}
+          >
+            <div class="flex flex-row">
+              <span class="prose-sm mr-5 font-light uppercase">
+                {props.leftSlot}{" "}
+              </span>
+              <span>{props.label}</span>
+              <span class="ml-auto">
+                <div class="relative mr-1 ml-4 flex flex-row">
+                  <AiFillDelete size={20} class="mt-0.5 text-gray-600" />
+                  {props.deletedAndHasDependentNodes ? (
+                    <AiFillClockCircle
+                      size={20}
+                      class="mt-0.5 ml-2 rounded-full text-warning"
+                    />
+                  ) : (
+                    <AiFillCheckCircle
+                      size={20}
+                      class="mt-0.5 ml-2 text-success"
+                    />
+                  )}
+                </div>
+              </span>
+            </div>
+          </UnsavedLink>
+        </Match>
+        <Match when={props.isDeleted}>
+          <span
+            class={
+              style +
+              `  w-fit select-none  bg-gray-400 pr-2 transition-all hover:bg-gray-500`
+            }
+            href={props.href}
+          >
+            <div class="flex flex-row">
+              <span class="prose-sm mr-5 font-light uppercase">
+                {props.leftSlot}{" "}
+              </span>
+              <span>{props.label}</span>
+              <span class="ml-auto">
+                <div class="relative mr-1 ml-4 flex flex-row">
+                  <AiFillDelete size={20} class="mt-0.5 text-gray-600" />
+                  {props.deletedAndHasDependentNodes ? (
+                    <AiFillClockCircle
+                      size={20}
+                      class="mt-0.5 ml-2 rounded-full text-warning"
+                    />
+                  ) : (
+                    <AiFillCheckCircle
+                      size={20}
+                      class="mt-0.5 ml-2 text-success"
+                    />
+                  )}
+                </div>
+              </span>
+            </div>
+          </span>
+        </Match>
+        <Match when={props.href}>
+          <UnsavedLink
+            class={
+              style +
+              ` cursor-pointer bg-primary transition-all hover:bg-primary-focus`
+            }
+            href={props.href}
+          >
             <span class="prose-sm mr-5 font-light uppercase">
               {props.leftSlot}{" "}
             </span>
-            <span>{props.label}</span>
-            <span class="ml-auto">
-              <div class="relative mr-1 ml-4 flex flex-row">
-                <AiFillDelete size={20} class="mt-0.5 text-gray-600" />
-                {props.deletedAndHasDependentNodes ? (
-                  <AiFillClockCircle
-                    size={20}
-                    class="mt-0.5 ml-2 rounded-full text-warning"
-                  />
-                ) : (
-                  <AiFillCheckCircle
-                    size={20}
-                    class="mt-0.5 ml-2 text-success"
-                  />
-                )}
-              </div>
-            </span>
-          </div>
-        </UnsavedLink>
-      </Match>
-      <Match when={props.isDeleted}>
-        <span
-          class={
-            style +
-            `  w-fit select-none  bg-gray-400 pr-2 transition-all hover:bg-gray-500`
-          }
-          href={props.href}
-        >
-          <div class="flex flex-row">
+            {props.label}
+          </UnsavedLink>
+        </Match>
+        <Match when={true}>
+          <span
+            class={
+              style +
+              ` cursor-pointer bg-primary hover:bg-primary-focus ` +
+              props.class
+            }
+            onMouseDown={props.onClick}
+          >
             <span class="prose-sm mr-5 font-light uppercase">
               {props.leftSlot}{" "}
             </span>
-            <span>{props.label}</span>
-            <span class="ml-auto">
-              <div class="relative mr-1 ml-4 flex flex-row">
-                <AiFillDelete size={20} class="mt-0.5 text-gray-600" />
-                {props.deletedAndHasDependentNodes ? (
-                  <AiFillClockCircle
-                    size={20}
-                    class="mt-0.5 ml-2 rounded-full text-warning"
-                  />
-                ) : (
-                  <AiFillCheckCircle
-                    size={20}
-                    class="mt-0.5 ml-2 text-success"
-                  />
-                )}
-              </div>
-            </span>
-          </div>
-        </span>
-      </Match>
-      <Match when={props.href}>
-        <UnsavedLink
-          class={
-            style +
-            ` cursor-pointer bg-primary transition-all hover:bg-primary-focus`
-          }
-          href={props.href}
-        >
-          <span class="prose-sm mr-5 font-light uppercase">
-            {props.leftSlot}{" "}
+            {props.label}
           </span>
-          {props.label}
-        </UnsavedLink>
-      </Match>
-      <Match when={true}>
-        <span
-          class={
-            style +
-            ` cursor-pointer bg-primary hover:bg-primary-focus ` +
-            props.class
-          }
-          onMouseDown={props.onClick}
-        >
-          <span class="prose-sm mr-5 font-light uppercase">
-            {props.leftSlot}{" "}
-          </span>
-          {props.label}
-        </span>
-      </Match>
-    </Switch>
+        </Match>
+      </Switch>
+    </>
   );
 };
 
